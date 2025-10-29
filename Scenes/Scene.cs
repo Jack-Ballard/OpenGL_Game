1848d44f591f2b52dfbb48d8d4613237bd3fda9c
Jack Ballard <jacksballard@gmail.com>
2025-10-29 10:19:45 +0000

    Added Game Over Scene

---
+++ Scenes/Scene.cs
@@ -0,0 +1,29 @@
+﻿using OpenTK.Windowing.Common;
+using OpenGL_Game.Managers;
+
+namespace OpenGL_Game.Scenes
+{
+    abstract class Scene
+    {
+        public enum SceneTypes
+        {
+            SCENE_NONE,
+            SCENE_MAIN_MENU,
+            SCENE_GAME,
+            SCENE_GAME_OVER
+        }
+
+        protected SceneManager sceneManager;
+
+        public Scene(SceneManager sceneManager)
+        {
+            this.sceneManager = sceneManager;
+        }
+
+        public abstract void Render(FrameEventArgs e);
+
+        public abstract void Update(FrameEventArgs e);
+
+        public abstract void Close();
+    }
+}
